1601a4107dd4001f137c1aa7f37906afde2b14bb
fadhilatur7 <fadhilatur7@gmail.com>
2020-04-25 08:10:33 +0700

    service management

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,84 @@
+<x-headerlogin/>
+<body style="background-color: #666666;">
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" action="{{ route('login') }}" method="POST">
+                @csrf
+                
+					<span class="login100-form-title p-b-43">
+						Login to continue
+					</span>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100 @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+						<span class="focus-input100"></span>
+                        <span class="label-input100">Email</span>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100 @error('password') is-invalid @enderror" type="password" name="password" required autocomplete="current-password>
+						<span class="focus-input100"></span>
+                        <span class="label-input100">Password</span>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+					</div>
+
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+							<label class="label-checkbox100" for="remember">
+								Remember me
+							</label>
+						</div>
+
+						<div>
+                        @if (Route::has('password.request'))
+							<a href="{{ route('password.request') }}" class="txt1">
+								Forgot Password?
+                            </a>
+                        @endif
+						</div>
+					</div>
+			
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+					
+					<div class="text-center p-t-46 p-b-20">
+						<span class="txt2">
+							or sign up using
+						</span>
+					</div>
+
+					<div class="login100-form-social flex-c-m">
+						<a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
+							<i class="fa fa-facebook-f" aria-hidden="true"></i>
+						</a>
+
+						<a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+
+				<div class="login100-more" style="background-image: url('assets/login/images/bg-01.jpg');">
+				</div>
+			</div>
+		</div>
+    </div>
+    <x-footerlogin/>
---
+++ resources/views/components/cardticket.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-6 col-xl-4">
+    <div class="card mb-3 widget-content bg-midnight-bloom">
+        <div class="widget-content-wrapper text-white">
+            <div class="widget-content-left">
+                <div class="widget-heading">Total Tickets</div>
+                    <div class="widget-subheading">Last year expenses</div>
+                </div>
+            <div class="widget-content-right">
+                <div class="widget-numbers text-white"><span>1896</span></div>
+            </div>
+        </div>
+    </div>
+</div>
